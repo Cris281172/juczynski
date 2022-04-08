501fb5598bb024f55895e8cd2cc11b7480af56c8
Cris281172 <krzysiu.juczynski@gmail.com>
2022-04-08 06:31:25 +0200

    Responsives

--- src/route/MainNav.js
+++ src/route/MainNav.js
@@ -2,15 +2,23 @@ import style from '../css/main-nav.module.scss'
 import { Link } from 'react-router-dom';
 import {useTranslation} from "react-i18next";
 import { GiHamburgerMenu } from "react-icons/gi";
+import {useRef} from 'react';
+
 const MainNav = () => {
 
     const { t, i18n } = useTranslation();
 
+    const list = useRef();
+
+    const listDisplay = () => {
+        console.log();
+        list.current.classList.toggle(`${style.active}`);
+    }
 
     return(
         <nav className={style.mainNav}>
-            <GiHamburgerMenu  className={style.hamburgerMenu} />
-            <ul className={style.navList}>
+            <GiHamburgerMenu  className={style.hamburgerMenu} onClick={() => listDisplay()} />
+            <ul className={style.navList} ref={list}>
                 <li className={style.item}>
                     <Link className={`${style.link} ${style.active}`}  to="/">{t('mainNav.home')}</Link>
                 </li>
