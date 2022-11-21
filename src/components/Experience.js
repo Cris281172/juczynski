c967af849e034cba9ae07f641aca137f57f845b3
Cris281172 <krzysiu.juczynski@gmail.com>
2022-11-21 22:01:39 +0100

    WIP

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,13 +3,8 @@ import style from '../css/expirence.module.scss'
 const Experience = () => {
     return(
         <div className={style.experience}>
-            <h3 className={style.firstTitle}>dasdsa</h3>
-            <span className={`${style.line} ${style.visible}`} />
-            <span className={style.line} />
-            <span className={style.line} />
-            <span className={style.line} />
-            <span className={style.line} />
-            <h3>dasdasdasdad</h3>
+            <h3 className={style.title}>A.G. Development | 01.06.2021 - 01.06.2022 | 1 rok</h3>
+            <h3 className={style.title}>BandIT House | 01.07.2022 - 01.09.2022 | 3 miesiące</h3>
         </div>
     )
 }
